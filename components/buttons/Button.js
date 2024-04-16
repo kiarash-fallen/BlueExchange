397a761e8f84fe9a0682983404dc49d872f3b32f
nasiri <futinasiri7@gmail.com>
2024-04-16 19:27:32 +0330

    button

--- components/buttons/Button.js
+++ components/buttons/Button.js
@@ -5,7 +5,7 @@ function Button({ children, color, hover, isDisable, type, size, href, hasLeftIc
   return (
     <>
       {href ? <Link
-        className={` bg-[#323232] border-b hover:border-b hover:text-white text-[#AAAAAA]  border-[#414141] 
+        className={`inline-block bg-[#323232] border-b hover:border-b hover:text-white text-[#AAAAAA]  border-[#414141] 
          rounded-[10px] px-5 py-2 group ${hover}`}
         href={href}>
         <span className='flex items-center'>
@@ -16,7 +16,7 @@ function Button({ children, color, hover, isDisable, type, size, href, hasLeftIc
 
         <span className={`w-1/3 h-2 bottom-0 blur-md rounded-xl mx-auto -mb-2 hidden group-hover:flex ${color}`}></span>
       </Link> : <button
-        className={` bg-[#323232] border-b hover:border-b hover:text-white text-[#AAAAAA]  border-[#414141] 
+        className={`inline-block bg-[#323232] border-b hover:border-b hover:text-white text-[#AAAAAA]  border-[#414141] 
          rounded-[10px] px-5 py-2 group ${hover}`}
         type={type}
         disabled={isDisable}
--- components/home/Header.js
+++ components/home/Header.js
@@ -1,15 +1,54 @@
+import Image from "next/image";
+import Button from "../buttons/Button";
 import IconContainer from "../ui/IconContainer";
+import Coins from "../../public/assests/images/coins.svg"
 
 const Header = () => {
-    return <header>
+    return <header className="grid grid-cols-2">
+        <div className="text-white">
+            <div className="flex justify-between">
+                <div className="self-center">
+                    <div className="flex items-center">
+                        <IconContainer color="bg-[#2D74FF]" border="border-[#2D74FF]">
+                            BEX
+                        </IconContainer>
+                        <p className="text-sm mr-4">هوشمند ترین پلتفرم صرافی دیجیتال</p>
+                    </div>
+                    <p className="font-semibold text-5xl mt-8">بیش از 125.025+</p>
+                </div>
+                <div className="">
+                    <Image src={Coins} width="auto" height={300} alt="coins" />
+                </div>
+            </div>
+            <p className="font-light text-[40px] mt-4">تعداد معامله گرانی که به  <span className="font-semibold">بلواکسچنج</span> اعتماد کردن</p>
+            <p className="font-normal text-[#B0B0B0]">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون
+            </p>
+            <p className="font-normal text-[#B0B0B0] mt-2">بلکه
+                روزنامه و مجله در ستون و سطرآنچنان که لازم است ...</p>
+
+            <div className="mt-10">
+                <Button
+                    href="/"
+                    varient="#2D74FF"
+                    color="bg-[#2D74FF]"
+                    hover="hover:border-[#2D74FF]">
+                    معامله تو شروع کن !
+                </Button>
+                <span className="mr-4">
+                    <Button
+                        href="/"
+                        varient="#2D74FF"
+                        color="bg-[#2D74FF]"
+                        hover="hover:border-[#2D74FF]">
+                        ارتباط با ما
+                    </Button>
+                </span>
+
+            </div>
+        </div>
         <div>
-            <div className="flex items-center">
-                <IconContainer color="bg-[#2D74FF]" border="border-[#2D74FF]">
-                    BEX
-                </IconContainer>
-                <p className="text-white mr-4">هوشمند ترین پلتفرم صرافی دیجیتال</p>
-                <p className="">بیش از 125.025+</p>
-                تعداد معامله گرانی که به بلواکسچنج اعتماد کردن
+            <div className="radius-[32px]">
+
             </div>
         </div>
     </header >
