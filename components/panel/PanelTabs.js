88c6a23ebbc039b582d2824d4316af3cef7a32fe
Kiarash <89937079+kiarash-fallen@users.noreply.github.com>
2024-05-11 22:24:07 +0330

    22th Commit v2

--- components/panel/PanelTabs.js
+++ components/panel/PanelTabs.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from 'react'
 
-function PanelTabs({ list }) {
+function PanelTabs({ list,widthFull,bgActive,childWidth }) {
     const [active, setActive] = useState(0);
     return (
         <>
